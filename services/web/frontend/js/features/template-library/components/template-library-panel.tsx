8963991bc707eb6af1d736df078bfc81dabec768
privaltr <38013722+privaltr@users.noreply.github.com>
2026-09-18 22:17:00 +0200

    Keep template title and insert button on one line

--- services/web/frontend/js/features/template-library/components/template-library-panel.tsx
+++ services/web/frontend/js/features/template-library/components/template-library-panel.tsx
@@ -508,6 +508,7 @@ export default function TemplateLibraryPanel() {
                     </Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
+                </div>
               </div>
             </div>
           ))
